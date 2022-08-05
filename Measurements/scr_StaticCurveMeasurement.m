77326df585dba83bcf68b5d30ffe9e3d87b432a9
LuchkaOnTheMoon <luchka.luconi@gmail.com>
2022-08-05 12:58:26 +0200

    - Static curve measurement script new version, computing I/O signals envelope using "exponential smoothing peak detector" with infinite release time and instantaneous attack time. - README.md added for measurements WAV folder.

--- Measurements/scr_StaticCurveMeasurement.m
+++ Measurements/scr_StaticCurveMeasurement.m
@@ -1,16 +1,32 @@
+%% Dynamic Range Compressor (DRC) static IN-OUT curve measurement script, for 
+%% GNU Octave (largely compatible with Matlab).
+%%
+%% Copyright (C) 2022 - Luca Novarini
+%% 
+%% This program is free software: you can redistribute it and/or modify it under
+%% the terms of the GNU General Public License as published by the Free Software
+%% Foundation, either version 3 of the License, or (at your option) any later
+%% version.
+%% 
+%% This program is distributed in the hope that it will be useful, but WITHOUT 
+%% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+%% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+%% details.
+%% 
+%% You should have received a copy of the GNU General Public License along with 
+%% this program. If not, see <https://www.gnu.org/licenses/>.
+
 clear all; close all; clc;
 
 %%%% USER PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-model = {'1176D'};
-ratio = [4, 8, 12, 20];
-knee = {'Default'};
+model = {'1176D'};          % '1176D', 'API2500', ...
+ratio = [4, 8, 12, 20];     % [adim]
+knee = {'Default'};         % 'Default', 'Hard', 'Soft', ...
+trimSize = 10000;           % [samples]
 
 %%%% PROCESSING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Import OctaveLib.
 addpath('./lib');
-
-% Load signal processing package (to access hilbert() function).
-pkg load signal;
 
 % For each model,
 for ii = 1:length(model)
@@ -36,18 +52,36 @@ for ii = 1:length(model)
 			input  = y(:, 1);
 			output = y(:, 2);
 
-			% Find both input/output signals envelope.
-			inputLevelInDb = 20 * log10(abs(hilbert(input)));
-			inputLevelInDb = inputLevelInDb(10000 : end - 10000);
-			outputLevelInDb = 20 * log10(abs(hilbert(output)));
-			outputLevelInDb = outputLevelInDb(10000 : end - 10000);
-			offsetIndex = find(inputLevelInDb >= (max(inputLevelInDb) + min(inputLevelInDb)) / 2, 1, 'first');
-			offset = inputLevelInDb(offsetIndex) - outputLevelInDb(offsetIndex);
+			% Calculate input signal envelope, exploiting the fact that its amplitude is constantly increasing.
+			inputEnvelope = zeros(length(input) + 1, 1);
+			for hh = 1:length(input)
+				if (abs(input(hh)) > inputEnvelope(hh))
+					inputEnvelope(hh + 1) = abs(input(hh));
+				else
+					inputEnvelope(hh + 1) = inputEnvelope(hh);
+				end
+			end
+			inputEnvelope = fcn_LinToDb20(inputEnvelope((2 + trimSize):(end - trimSize)));
+            
+			% Calculate output signal envelope, exploiting the fact that its amplitude is constantly increasing.
+			outputEnvelope = zeros(length(output) + 1, 1);
+			for hh = 1:length(output)
+				if (abs(output(hh)) > outputEnvelope(hh))
+					outputEnvelope(hh + 1) = abs(output(hh));
+				else
+					outputEnvelope(hh + 1) = outputEnvelope(hh);
+				end
+			end
+			outputEnvelope = fcn_LinToDb20(outputEnvelope((2 + trimSize):(end - trimSize)));
+            
+			% Compensate for any potential IN/OUT gain offset.
+			offsetIndex = min(find(inputEnvelope >= (max(inputEnvelope) + min(inputEnvelope)) / 2));
+			offset = inputEnvelope(offsetIndex) - outputEnvelope(offsetIndex);
 
-			% Display signal and envelope.
-			plot(inputLevelInDb - offset, outputLevelInDb, fcn_LineSpecByIndex(jj + kk - 1));
+			% Plot static curve.
+			plot(inputEnvelope - offset, outputEnvelope, fcn_LineSpecByIndex(jj + kk - 1));
 			
-			% Generate legend string for this plot.
+			% Append legend string for current plot to legend strings cell.
 			legendStrings{jj + kk - 1} = sprintf('Ratio %d, Knee %s', ratio(jj), knee{kk});
 		end
 	end
---
+++ Measurements/wav/README.md
@@ -0,0 +1,5 @@
+# Static Curve Measurements
+All .wav files in this folder must be stereo and contain IN signal track (1kHz amplitude sweep) on LEFT channel, and recorded dynamic range compressor OUT signal track on RIGHT channel.
+File naming convention is the following: 
+{Compressor name or model}_Ratio_{Selected compression ratio}_Knee_{Default, Soft, Hard, ...}.wav
+Attack and release times must be set to the lowest possible values available, and threshold, input gain, output gain, make-up gain must be set to achieve an undistorted output signal and an appreciable gain reduction.
